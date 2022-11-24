09a73dd2afbf4b94296ade57c84cb2bec223eecf
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 07:25:01 +0530

    Added logi to find the player roll the die to get a number Between 1 to 6

--- src/com/java/snakeandladder/SnakeAndLadder.java
+++ src/com/java/snakeandladder/SnakeAndLadder.java
@@ -1,15 +1,22 @@
 package com.java.snakeandladder;
+
+import java.util.Random;
+
 /**
- * Snake and Ladder game played with single player at start position 0.
+ * The Player rolls the die to get a number Between 1 to 6.
  */
 
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+		Random Ra = new Random();
 		System.out.println("Welcome to the Snake And Ladder Program");
 		int userPosition = 0;
 		System.out.println("----Game Starts----");
 		System.out.println("Initial position of user: "+userPosition);
+		
+		int dice = Ra.nextInt(6) + 1 ;
+		System.out.println("Dice Rolled: "+dice);
 	}
 
 }
