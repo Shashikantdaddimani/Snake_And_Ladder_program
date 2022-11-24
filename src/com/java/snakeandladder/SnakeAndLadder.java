a3337ac52a702addbca0a88e1e9e42a990a696af
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 07:45:03 +0530

    Added logic to find Till The Player reaches the winning position 100

--- src/com/java/snakeandladder/SnakeAndLadder.java
+++ src/com/java/snakeandladder/SnakeAndLadder.java
@@ -3,7 +3,7 @@ package com.java.snakeandladder;
 import java.util.Random;
 
 /**
- * The Player then checks for a Option They are No Play,Ladder and Snake
+ * Repeat till the Player reaches the winning position 100.
  */
 
 public class SnakeAndLadder {
@@ -12,9 +12,13 @@ public class SnakeAndLadder {
 		Random Ra = new Random();
 		System.out.println("Welcome to the Snake And Ladder Program");
 		int userPosition = 0;
+		int count = 0;
 		System.out.println("----Game Starts----");
 		System.out.println("Initial position of user: "+userPosition);
 		
+		while(userPosition != 100)
+		{
+			count++;
 		int dice = Ra.nextInt(6) + 1 ;
 		System.out.println("Dice Rolled: "+dice);
 		
@@ -31,10 +35,13 @@ public class SnakeAndLadder {
 				userPosition = userPosition - dice;
 				
 				if(userPosition<0)
-				{ userPosition = 0; 
+				{ 
+					userPosition = 0; 
 				}
 		}
 		System.out.println("Presnt Position of the User: "+userPosition);
+		}
+		System.out.println("User has rolled "+count+" times to win!");
 	}
 
 }
