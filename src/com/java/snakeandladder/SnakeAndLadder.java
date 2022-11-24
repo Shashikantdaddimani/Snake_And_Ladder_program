8f93340cd346c6173d40f032c3de49a5c747ad63
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 09:33:44 +0530

    Added logic to find the number of times the dice was played to win the game

--- src/com/java/snakeandladder/SnakeAndLadder.java
+++ src/com/java/snakeandladder/SnakeAndLadder.java
@@ -3,48 +3,49 @@ package com.java.snakeandladder;
 import java.util.Random;
 
 /**
- * Ensure the player gets to exact winning position 100.
+ * Report the number of times the dice was played to win the game and also the position
+after every die role
  */
 
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		Random Ra = new Random();
+		Random random = new Random();
 		System.out.println("Welcome to the Snake And Ladder Program");
 		int userPosition = 0;
 		int count = 0;
 		System.out.println("----Game Starts----");
 		System.out.println("Initial position of user: "+userPosition);
 		
-		while(userPosition != 100)
-		{
+		while (userPosition != 100) {
 			count++;
-		int dice = Ra.nextInt(6) + 1 ;
-		System.out.println("Dice Rolled: "+dice);
-		
-		int option = Ra.nextInt(3);
-		switch(option)
-		{
-		case 0: System.out.println("No Play!!");
+			int dice = random.nextInt(6) + 1;
+			System.out.println("Dice Rolled: " + dice);
+
+			int option = random.nextInt(3);
+			switch (option) {
+			case 0:
+				System.out.println("No Play!!");
 				break;
-		case 1: System.out.println("Ladder!");
+			case 1:
+				System.out.println("Ladder!");
 				userPosition = userPosition + dice;
-				if(userPosition >100)
-				{
+				if (userPosition > 100) {
 					userPosition -= dice;
 				}
 				break;
-		default: System.out.println("Snake!");
+			default:
+				System.out.println("Snake!");
 				userPosition = userPosition - dice;
-				
-				if(userPosition<0)
-				{ 
-					userPosition = 0; 
+
+				if (userPosition < 0) {
+					userPosition = 0;
 				}
+			}
+			System.out.println("Number of times user played "+count+" to reach: "+userPosition);
+			System.out.println("Present Position of the User: " + userPosition);
 		}
-		System.out.println("Present Position of the User: "+userPosition);
-		}
-		System.out.println("User has rolled "+count+" times to win!");
+		System.out.println("User has rolled " + count + " times to win!");
 	}
 
 }
